ac3035735d356ddae38ae9143a603c95d7083906
Hossein Masjedi <120756379+xCamelot2001@users.noreply.github.com>
2024-03-18 17:41:59 +0000

    Add CameraControl component

---
+++ frontend/src/CameraControl.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef, useState } from 'react';
+import useCamera from './useCamera';
+import CameraFeed from './CameraFeed';
+import DisplayEmotion from './DisplayEmotion';
+import { Box } from '@mui/material';
+
+const CameraControl = () => {
+  const { videoRef } = useCamera();
+  const [emotion, setEmotion] = useState('');
+  const intervalRef = useRef(null);
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      if (videoRef.current) {
+        capture(videoRef.current).then(imageSrc => {
+          fetch('http://127.0.0.1:5000/predict', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ image: imageSrc }),
+          })
+          .then((res) => res.json())
+          .then((data) => {
+            setEmotion(data.emotion);
+          });
+        });
+      }
+    }, 1000);
+
+    return () => clearInterval(intervalRef.current);
+  }, [videoRef]);
+
+  const capture = (video) => {
+    return new Promise((resolve) => {
+      const canvas = document.createElement('canvas');
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      resolve(canvas.toDataURL('image/png'));
+    });
+  };
+
+  return (
+    <Box>
+      <DisplayEmotion emotion={emotion} />
+      <CameraFeed videoRef={videoRef} />
+    </Box>
+  );
+};
+
+export default CameraControl;
